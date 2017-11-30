86d8edbb4a7adc50775aaa0b319c6ba51261c5be
Isaac <qwertytrogi@gmail.com>
2017-11-30 11:01:09 -0600

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -43,7 +43,7 @@ def page_main():
       except:
          return "Unknown Error"
       
-   files = [{"name": i, "size": os.path.getsize("/filehost/files/qwerty"+i)} for i in os.listdir("/filehost/files/qwerty")]
+   files = [{"name": i, "size": os.path.getsize("/filehost/files/qwerty/"+i)} for i in os.listdir("/filehost/files/qwerty")]
    return render_template("mainpage.html", files=files)
                   
 
